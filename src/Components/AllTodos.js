fa631ce4326bb91d34e8eab213c96929177f90be
enPlace <n.place@protonmail.com>
2021-08-30 14:03:03 -0300

    Setup API fetch

--- src/Components/AllTodos.js
+++ src/Components/AllTodos.js
@@ -1,5 +1,17 @@
+import { useEffect } from "react";
+import { useState } from "react";
+import axios from "axios";
+
 const AllTodos = () => {
-    return ( <div>these are multiple todos</div> );
-}
- 
+  const [todos, setTodos] = useState(undefined);
+  useEffect(() => {
+    axios.get(`https://jsonplaceholder.typicode.com/todos`).then((res) => {
+      const responseTodos = res.data;
+      setTodos(responseTodos);
+    });
+  }, []);
+  useEffect(()=>console.log(todos), [todos])
+  return <div>these are multiple todos</div>;
+};
+
 export default AllTodos;
